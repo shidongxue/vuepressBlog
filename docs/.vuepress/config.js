9d19adf70d4f9d654c4f2758c622877517fe4b2b
shidongxue <3342165290@qq.com>
2021-06-12 11:46:11 +0800

    模板配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'Chen\'s blog',
-  description: '我的个人网站',
+  title: '前端小白',
+  description: '个人网站',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
@@ -14,7 +14,45 @@ module.exports = {
       {text: '算法题库', link: '/algorithm/'},
       {text: '微博', link: 'https://baidu.com'}      
     ],
-    sidebar: 'auto', // 侧边栏配置
+    sidebar: {
+      '/css/': [{
+          title: 'CSS样式',
+          collapsable: true, 
+          children: [
+            { title: '布局', path:'/css/布局'},
+            { title: '相对定位与绝对定位', path:'/css/相对定位与绝对定位'},
+          ]
+      }],
+      '/html/': [{
+          title: 'HTML',
+          collapsable: false, 
+          children: [
+            { title: 'form表单提交', path:'/html/form表单提交'},
+          ]
+      }],
+      '/js/': [{
+          title: 'javascript',
+          collapsable: false, 
+          children: [
+            { title: 'items01', path:'/js/ECMA/作用域'},
+          ]
+      }],
+      '/NodeJs/': [{
+          title: 'NodeJs',
+          collapsable: false, 
+          children: [
+            { title: 'items01', path:'/NodeJs/'},
+          ]
+      }],
+       '/Vue/': [{
+          title: 'Vue知识点',
+          collapsable: false, 
+          children: [
+            { title: 'items01', path:'/Vue/'},
+          ]
+      }],
+    },
     sidebarDepth: 2, // 侧边栏显示2级
+    displayAllHeaders: true,
   }
 };
